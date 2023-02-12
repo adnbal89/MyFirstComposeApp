ae0b63f3384cf043a49c596fb019f0f49ca134ae
Adnan BAL <adnbal89@hotmail.com>
2023-02-12 17:56:24 +0300

    - Color palette implementation

--- app/src/main/java/com/aceofhigh/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/aceofhigh/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.aceofhigh.borutoapp.presentation.screens.home
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -8,6 +9,8 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
 import com.aceofhigh.borutoapp.navigation.Screen
 import com.aceofhigh.borutoapp.presentation.common.ListContent
+import com.aceofhigh.borutoapp.ui.theme.statusBarColor
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @ExperimentalCoilApi
 @Composable
@@ -16,6 +19,11 @@ fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(
+        color = MaterialTheme.colors.statusBarColor
+    )
 
     Scaffold(
         topBar = {
